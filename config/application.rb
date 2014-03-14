2fa0ffcd2265ee264caede4e79a4665a2ed66652
William Mantegna <wmantegna@gmail.com>
2014-03-14 01:04:59 -0400

    s3 addition

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Salsa
       Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+
+      config.assets.initialize_on_precompile = false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
