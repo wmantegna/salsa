c803c696f891c898f799da68d28ec0e945a82293
William Mantegna <wmantegna@gmail.com>
2014-03-14 00:38:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salsa::Application.config.secret_key_base = 'f5c59707cc0537d381c577375c91933f415f5c14583edc09a5f40e8be3323b0a01a5e6c2eadedd86224925b4a7791c13b5385fade91800c7924fcd7fd791d256'
